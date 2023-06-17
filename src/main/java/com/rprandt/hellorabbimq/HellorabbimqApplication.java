b79e41e91a94fae6c836f0e3348774a6de54d72c
Richard Prandt <rprandt.contato@gmail.com>
2023-06-17 09:21:57 -0300

    first commit

---
+++ src/main/java/com/rprandt/hellorabbimq/HellorabbimqApplication.java
@@ -0,0 +1,13 @@
+package com.rprandt.hellorabbimq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellorabbimqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellorabbimqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rprandt/hellorabbimq/HellorabbimqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rprandt.hellorabbimq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellorabbimqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
